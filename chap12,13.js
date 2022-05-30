31f6cd57d83a604c7411204177d4913d737b6ba6
subhanrizwan0 <subhanahmed444sherazi@gmail.com>
2022-05-30 09:33:40 +0500

    assignment-4 js

---
+++ chap12,13.js
@@ -0,0 +1,68 @@
+
+
+// question 2
+// var a= prompt("enter the integers");
+// var b=prompt("enter the integers");
+// if(a < b){
+//     alert(b + " " + "is larger")
+// }else if(a > b){
+//     alert(a + " " + "is larger")
+// }
+// else if(a===b){
+//     alert( "equal")
+// }
+
+// question 3
+// var a=prompt("enter the integers")
+// if(a>0){
+//     alert("the number is +ve");
+// }else if(a<0)
+//     {   alert("the number is negative");
+// }else{
+//     alert("the number is zero");
+// }
+
+// question 4
+// var v=prompt("enetr the vowels");
+// if (v==="i"){
+//     var v=prompt("enetr the vowels")
+//      if(v==="e"){}
+//      var v=prompt("enetr the vowels")
+//      if(v==="o"){}
+//      var v=prompt("enetr the vowels")
+//      if(v==="u"){}
+//      var v=prompt("enetr the vowels")
+//      if(v==="a"){}
+//     alert("true")
+// }else{alert("false")}
+
+// question 5
+// var p1=prompt("enter the password 1")
+// var p2=prompt("enter the password 2")
+// if(p1 && p2 === "subhanrizwan"){
+//     alert("correct password")
+// }else{alert("incorrect password")}
+
+// // question 6
+// var hour= prompt("18")
+// if(hour <= 18){
+// alert("good day")
+// }else{alert("good evening")}
+
+// question 7
+// var hour=prompt("enter the time format like 1900 = 7pm")
+// if (hour<0 || hour>"2400"){
+// alert("enter the time in 2400")
+// }
+// if(hour <00000 && hour < 1200 ){
+//     alert("good morning")
+// }
+// if(hour >=1200 && hour < 1700 ){
+//     alert("good afternoon")
+// }
+// if(hour >= 1700 && hour < 1900 ){
+//     alert("good evening")
+// }
+// if(hour >=1900 && hour <= 2400 ){
+//     alert("good night")
+// }
