af02a8e2ba1eb0ed11721605df358e3307ea7aec
subhanrizwan0 <subhanahmed444sherazi@gmail.com>
2022-06-03 09:16:33 +0500

    update

---
+++ chap14,15.js
@@ -0,0 +1,141 @@
+ // question 1
+// var cities=[]
+// cities[1]="karachi"
+// console.log(cities);
+
+// question 3,4,5,6
+// var cities=["karachi" , 03343760625 , true , "subhan012"]
+// console.log(cities);
+
+// question 7
+// var ed=["SSC", "HSC", "BCS","BS", "BCOM", "MS", "MPhil", "PhD"]
+// var qul=prompt("education qualification in pakistan")
+// alert(ed)
+// console.log(ed);
+// document.write("<h1>education qualification in pakistan</h1>")
+// document.write("SSC" + "<br>")
+// document.write("HSC" + "<br>")
+// document.write("BCS" + "<br>")
+// document.write("BS" + "<br>")
+// document.write("BCOM" + "<br>")
+// document.write("MS" + "<br>")
+// document.write("MPhill" + "<br>")
+// document.write("phD" + "<br>")
+
+// question 8
+// var st_1=["mustufa"]
+// var score=355
+// var totalscore=500
+// var per=[(score/totalscore)*100]
+// console.log(st_1);
+// console.log(per); 
+// document.write("score of mustufa is "+ score + " percentage is " + per  )
+
+// var st_2=["shayan"]
+// var score=300
+// var totalscore=500
+// var per=[(score/totalscore)*100]
+// console.log(st_2);
+// console.log(per);
+// document.write("score of shayan is "+ score + " percentage is " + per  )
+
+// var st_3=["ahmed"]
+// var score=246
+// var totalscore=500
+// var per=[(score/totalscore)*100]
+// console.log(st_3);
+// console.log(per);
+// document.write("score of ahmed is "+ score + " percentage is " + per  )
+
+// question 9
+// var colors=["red" , "green" , "yellow" , "blue" , "pink"]
+// var clr=prompt("colors name?")
+// document.write("colors names"+ "</br>" + colors)
+// colors.unshift("black")
+// console.log(colors);
+
+// var st_add=prompt("which color add at starting")
+// var colors=["red" , "green" , "yellow" , "blue" , "pink"]
+
+// document.write("<h2> add color at starting </h2>")
+// document.write(st_add)
+// console.log(colors.unshift("black"));
+// console.log(colors);
+
+// var lastadd=prompt("which color add at starting")
+// var colors=["red" , "green" , "yellow" , "blue" , "pink"]
+
+// document.write("<h2> add color at last </h2>")
+// document.write(lastadd)
+// console.log(colors.push("black"));
+// console.log(colors);
+
+// var st_rmv=prompt("which color add at starting")
+// var colors=["red" , "green" , "yellow" , "blue" , "pink"]
+
+// document.write("<h2> remove color at starting </h2>")
+// document.write( st_rmv)
+// console.log(colors.shift("black"));
+// console.log(colors);
+
+// var last_rmv=prompt("which color add at starting")
+// var colors=["red" , "green" , "yellow" , "blue" , "pink"]
+
+// document.write("<h2> remove color at last </h2>")
+// document.write( last_rmv)
+// console.log(colors.pop("black"));
+// console.log(colors);
+
+// var colors=["red","green","yellow","blue","pink"]
+// var user=prompt("enter the removed color??")
+// document.write(colors.splice(user,1))
+// // console.log(colors.splice(user,1));
+// console.log(colors);
+
+// var colors=["red","green","yellow","blue","pink"]
+// var user=prompt("enter the removed color")
+// var user1=prompt("enter the add color")
+// document.write(colors.splice(user,1,"white"))
+// console.log(colors.splice(user,1,user1));
+// console.log(colors);
+
+
+
+// question 11
+// var cities=["karachi" , "lahore" ,"islalamabad" ,"quetta" ,"peshawar" ]
+// console.log(cities.slice(2,4))
+// document.write(cities.slice(2,4))
+// console.log(cities);
+
+
+// question#12
+// var arr=["this", "is", "subhan", "ahmed"]
+// var user=prompt("enetr the arr")
+
+// console.log(arr.join(user));
+// console.log(arr.join(" "));
+// console.log(arr);
+
+// question#13
+// var arr=["keyboard","mouse","printer","monitor"]
+// console.log(arr);
+
+
+// question#14
+// var arr=["keyboard","mouse","printer","monitor"]
+// console.log(arr.reverse());
+
+// quetion#15
+// var arr=["apple","samsung","motorola","nokia","soni","heir"]
+// document.write(arr[0] +"<br>" )
+// document.write(arr[1] +"<br>" )
+// document.write(arr[2] +"<br>" )
+// document.write(arr[3] +"<br>" )
+// document.write(arr[4] +"<br>" )
+// document.write(arr[5] +"<br>" )
+
+
+
+ 
+
+
